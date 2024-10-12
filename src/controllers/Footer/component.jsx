db6c4717621d6c894c2ebb0e8328a655ec47f8dc
Zohaib Akhtar <zagalaxycode@gmail.com>
2024-10-12 15:02:24 +0500

    First Commit

---
+++ src/controllers/Footer/component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+function Footer() {
+  return (
+    <div className="footer">
+      Created By Zohaib Akhtar &nbsp; <span className="footer__heart">❤</span> &nbsp;
+       &nbsp;
+    </div>
+  );
+}
+
+export default Footer;
